91f9d799934ab9d8d3bc1bf29bc5538294178adc
TitiAlone <titi_alone@gmx.fr>
2018-07-17 16:50:47 +0200

    First commit :tada:

---
+++ app.js
@@ -0,0 +1,5 @@
+const mw = require("nodemw");
+
+// Load your own config file here, or use default (warning: with IP)
+// const bot = new mw("config/default.js");
+const bot = new mw("config/talone.js");
